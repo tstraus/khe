db621398b6d00353cec0cd1675ab06d3092de430
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2018-06-09 14:52:18 -0400

    built on mac

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 echo "----- building server -----"
 shards install
 crystal build --release ./src/khe.cr
+echo "----- done server -----"
 
 echo "----- building website -----"
 npm install
 npm run build
+echo "----- done website -----"
